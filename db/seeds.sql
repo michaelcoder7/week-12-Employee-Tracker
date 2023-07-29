36abfc1ff0e9c056e7f3446f366de130297be444
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-07-29 02:54:22 +0100

    seeds in progress

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (id, name) VALUES (1, 'Sales');
+INSERT INTO department (id, name) VALUES (2, 'Engineering');
+INSERT INTO department (id, name) VALUES (3, 'Finance');
+INSERT INTO department (id, name) VALUES (4, 'Legal');
+
+INSERT INTO role (id, title, salary, department_id) VALUES (1, 'Sales Lead', 100000, 1);
+INSERT INTO role (id, title, salary, department_id) VALUES (2, 'Salesperson', 80000, 1);
+INSERT INTO role (id, title, salary, department_id) VALUES (3, 'Lead Engineer', 200000, 2);
+INSERT INTO role (id, title, salary, department_id) VALUES (4, 'Software Engineer', 180000, 2);
+INSERT INTO role (id, title, salary, department_id) VALUES (5, 'Accountant', 150000, 3);
+INSERT INTO role (id, title, salary, department_id) VALUES (6, 'Legal Team Lead', 200000, 4);
+INSERT INTO role (id, title, salary, department_id) VALUES (7, 'Lawyer', 180000, 4);
