ce9bdd9ef7842afd053724f71384cfafd2fad5cb
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-07-29 02:25:38 +0100

    questions.js in proress

--- questions.js
+++ questions.js
@@ -19,8 +19,56 @@ const AddDepartmentQuestions = [
   {
     type: "input",
     name: "department_name",
-    message: "Enter the name of the New Department....",
+    message: "Enter the name of the New Department",
   },
 ];
 
-const AddRoleQuestions = [];
+const AddRoleQuestions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Enter the Title of the New Role",
+  },
+  {
+    type: "number",
+    name: "salary",
+    message: "Enter the Salary of the New Role (must be numeric)",
+    validate: function (value) {
+      const valid = !isNaN(parseInt(value));
+      return valid || "Please enter a number";
+    },
+  },
+  {
+    type: "list",
+    name: "department_id",
+    message: "Select the Department for the Role",
+    choices: [],
+  },
+];
+
+const AddEmployeeQuestions = [
+  {
+    type: "input",
+    name: "first_name",
+    message: "Enter the First Name of the Employee",
+  },
+  {
+    type: "input",
+    name: "last_name",
+    message: "Enter the Last Name of the Employee",
+  },
+  {
+    type: "list",
+    name: "role_id",
+    message: "Select the role for the Employee",
+    choices: [],
+  },
+  {
+    type: "list",
+    name: "manager_id",
+    message: "Select the Employee's Manager",
+    choices: [],
+  },
+];
+
+const UpdateEmployeeRoleQuestions = [{}];
