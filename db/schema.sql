8c24a2aa5b7aecdc497d8dc665c7d65711d56b38
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-07-29 02:40:08 +0100

    schema in progress

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department
+(
+    id INT NOT NULL
+    AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (30)
+);
+
+CREATE TABLE role
+(
+    id INT NOT NULL
+    AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
