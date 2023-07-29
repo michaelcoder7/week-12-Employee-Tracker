29817e78da12e9e7764a107710a54911e821e797
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-07-29 03:08:34 +0100

    seeds in progress

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,3 +10,14 @@ INSERT INTO role (id, title, salary, department_id) VALUES (4, 'Software Enginee
 INSERT INTO role (id, title, salary, department_id) VALUES (5, 'Accountant', 150000, 3);
 INSERT INTO role (id, title, salary, department_id) VALUES (6, 'Legal Team Lead', 200000, 4);
 INSERT INTO role (id, title, salary, department_id) VALUES (7, 'Lawyer', 180000, 4);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (1, 'John', 'Deacon', 1, NULL);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (2, 'John', 'Entwhistle', 2, 1);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (3, 'Jaco', 'Pastorius', 3, NULL);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (4, 'Mark', 'King', 4, 3);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (5, 'Marcus', 'Miller', 5, NULL);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (6, 'Stanley', 'Clarke', 6, NULL);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (7, 'Victor', 'Wooten', 7, 6);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (8, 'Flea', 'Smith', 2, 1);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (9, 'Cliff', 'Burton', 4, 3);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (10, 'Phil', 'Lynott', 7, 6);
