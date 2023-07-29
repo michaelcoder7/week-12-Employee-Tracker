693186303214e7624f521e21977e8713a61c445a
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-07-29 02:44:38 +0100

    Schema completed

--- db/schema.sql
+++ db/schema.sql
@@ -21,3 +21,16 @@ CREATE TABLE role
     REFERENCES department(id)
     ON DELETE SET NULL
 );
+
+CREATE TABLE employee
+(
+    id INT NOT NULL 
+    AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30),
+    last_name VARCHAR (30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role (id)
+    ON DELETE SET NULL
+);
