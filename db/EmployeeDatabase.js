f03e5470236a5e49c0914db65da80fd56eafbd81
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-11 12:15:47 +0100

    employee database in progress

--- db/EmployeeDatabase.js
+++ db/EmployeeDatabase.js
@@ -18,7 +18,26 @@ class EmployeeDatabase extends Database {
 
   getRoles() {
     return new Promise((resolve, reject) => {
-      this.db.query(`SELECT role.id, `);
+      this.db.query(
+        `SELECT role.id, role.title, CONCAT('£', FORMAT(salary, 0), ' p/a') as salary, department.name as department_name FROM role INNER JOIN Deartment ON role.department_id = Department.id`,
+        (err, results) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(results);
+        }
+      );
+    });
+  }
+
+  getEmployees() {
+    return new Promise((resolve, reject) => {
+      this.db.query(
+        `SELECT
+           employee.id,
+           CONCAT(employee.first_name, ' ', employee.last_name) as name,
+           `
+      );
     });
   }
 }
