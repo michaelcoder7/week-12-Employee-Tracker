711286ba70b7d50ca9bcae411db27a90b3793cd2
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-11 16:02:37 +0100

    necessary changes made

--- db/EmployeeDatabase.js
+++ db/EmployeeDatabase.js
@@ -19,7 +19,7 @@ class EmployeeDatabase extends Database {
   getRoles() {
     return new Promise((resolve, reject) => {
       this.db.query(
-        `SELECT role.id, role.title, CONCAT('£', FORMAT(salary, 0), ' p/a') as salary, department.name as department_name FROM role INNER JOIN Deartment ON role.department_id = Department.id`,
+        `SELECT role.id, role.title, CONCAT('£', FORMAT(salary, 0), ' p/a') as salary, department.name as department_name FROM role INNER JOIN Department ON role.department_id = Department.id`,
         (err, results) => {
           if (err) {
             reject(err);
