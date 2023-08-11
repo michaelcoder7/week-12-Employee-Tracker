71f245d671830ecd6290da60f6084d1395a334a1
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-11 07:55:46 +0100

    index.js file in process

--- index.js
+++ index.js
@@ -14,3 +14,32 @@ const db = new EmployeeDatabase({
   password: "SQLSoftwarePassword2023*",
   database: "employee_db",
 });
+
+db.connect();
+
+const doMenuQuestions = () => {
+
+    inquirer
+        .prompt(MainMenuQuestions)
+        .then((response)) => {
+
+            switch (response.option) {
+                case "view_departments":
+                    view_departments();
+                    break;
+                case "view_roles":
+                    view_roles();
+                    break;
+                case "view_employees":
+                     view_employees();
+                    break;
+                case "add_department":
+                    add_department();
+                    break;
+                case "add_role":
+                    add_role();
+                    break;    
+
+            }
+        }
+}
